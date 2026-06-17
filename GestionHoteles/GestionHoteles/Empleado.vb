96f052e9b51b448b02519c6730dcc153cdf8dfc0
Victor2005-18 <mvictormanuel633@gmail.com>
2026-06-17 14:44:35 -0600

    primer commit (1/3)

---
+++ GestionHoteles/GestionHoteles/Empleado.vb
@@ -0,0 +1,130 @@
+﻿Public MustInherit Class Empleado
+    'Campos privados
+    Private _nombre As String
+    Private _rfc As String
+    Private _salarioBase As Decimal
+    Private _departamento As String
+
+    'Campo compartido
+    Private Shared _totalEmpleados As Integer = 0
+
+    'Constructor
+    Public Sub New(nombre As String,
+                   rfc As String,
+                   salarioBase As Decimal,
+                   departamento As String)
+
+        Me.Nombre = nombre
+        Me.RFC = rfc
+        Me.SalarioBase = salarioBase
+        Me.Departamento = departamento
+
+        _totalEmpleados += 1
+    End Sub
+
+    'Propiedad Nombre
+    Public Property Nombre As String
+        Get
+            Return _nombre
+        End Get
+        Set(value As String)
+
+            If Not EsNombreValido(value) Then
+                Throw New Exception("Nombre inválido.")
+            End If
+
+            _nombre = value
+        End Set
+    End Property
+
+    'Propiedad RFC
+    Public Property RFC As String
+        Get
+            Return _rfc
+        End Get
+        Set(value As String)
+
+            If Not EsRFCValido(value) Then
+                Throw New Exception("RFC inválido. Debe tener 13 caracteres en mayúsculas.")
+            End If
+
+            _rfc = value
+        End Set
+    End Property
+
+    'Propiedad SalarioBase
+    Public Property SalarioBase As Decimal
+        Get
+            Return _salarioBase
+        End Get
+        Set(value As Decimal)
+
+            If Not EsSalarioValido(value) Then
+                Throw New Exception("El salario debe ser mayor a 0 y menor o igual a 500,000.")
+            End If
+
+            _salarioBase = value
+        End Set
+    End Property
+
+    'Propiedad Departamento
+    Public Property Departamento As String
+        Get
+            Return _departamento
+        End Get
+        Set(value As String)
+            _departamento = value
+        End Set
+    End Property
+
+    'Propiedad de solo lectura
+    Public ReadOnly Property NombreCompleto As String
+        Get
+            Return $"{Nombre} ({RFC})"
+        End Get
+    End Property
+
+    'Método abstracto
+    Public MustOverride Function CalcularPagoMensual() As Decimal
+
+    'Método virtual
+    Public Overridable Function ObtenerFicha() As String
+
+        Return $"Nombre: {Nombre}" & vbCrLf &
+               $"RFC: {RFC}" & vbCrLf &
+               $"Departamento: {Departamento}" & vbCrLf &
+               $"Salario Base: {FormatearMoneda(SalarioBase)}"
+    End Function
+
+    'Métodos auxiliares de validación y formato
+    Private Function EsNombreValido(value As String) As Boolean
+        If String.IsNullOrWhiteSpace(value) Then
+            Return False
+        End If
+
+        Return value.Trim().Length >= 3
+    End Function
+
+    Private Function EsRFCValido(value As String) As Boolean
+        If String.IsNullOrWhiteSpace(value) Then
+            Return False
+        End If
+
+        Dim r As String = value.Trim()
+        Return r.Length = 13 AndAlso r = r.ToUpperInvariant()
+    End Function
+
+    Private Function EsSalarioValido(value As Decimal) As Boolean
+        Return value > 0D AndAlso value <= 500000D
+    End Function
+
+    Private Function FormatearMoneda(value As Decimal) As String
+        Return value.ToString("C2", System.Globalization.CultureInfo.CurrentCulture)
+    End Function
+
+    'Método Shared
+    Public Shared Function GetTotal() As Integer
+        Return _totalEmpleados
+    End Function
+
+End Class
---
+++ GestionHoteles/GestionHoteles/Form1.Designer.vb
@@ -0,0 +1,183 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        DataGridView1 = New DataGridView()
+        nombre = New DataGridViewTextBoxColumn()
+        rfc = New DataGridViewTextBoxColumn()
+        tipo = New DataGridViewTextBoxColumn()
+        departamento = New DataGridViewTextBoxColumn()
+        pago_mensual = New DataGridViewTextBoxColumn()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        Label2 = New Label()
+        Label3 = New Label()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(346, 30)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(78, 25)
+        Label1.TabIndex = 0
+        Label1.Text = "Principal"
+        ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Columns.AddRange(New DataGridViewColumn() {nombre, rfc, tipo, departamento, pago_mensual})
+        DataGridView1.Location = New Point(1, 42)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.RowHeadersWidth = 62
+        DataGridView1.Size = New Size(818, 225)
+        DataGridView1.TabIndex = 1
+        ' 
+        ' nombre
+        ' 
+        nombre.HeaderText = "Nombre"
+        nombre.MinimumWidth = 8
+        nombre.Name = "nombre"
+        nombre.Width = 150
+        ' 
+        ' rfc
+        ' 
+        rfc.HeaderText = "RFC"
+        rfc.MinimumWidth = 8
+        rfc.Name = "rfc"
+        rfc.Width = 150
+        ' 
+        ' tipo
+        ' 
+        tipo.HeaderText = "Tipo (Gerente/Operatario)"
+        tipo.MinimumWidth = 8
+        tipo.Name = "tipo"
+        tipo.Width = 150
+        ' 
+        ' departamento
+        ' 
+        departamento.HeaderText = "Departamento"
+        departamento.MinimumWidth = 8
+        departamento.Name = "departamento"
+        departamento.Width = 150
+        ' 
+        ' pago_mensual
+        ' 
+        pago_mensual.HeaderText = "Pago Mensual"
+        pago_mensual.MinimumWidth = 8
+        pago_mensual.Name = "pago_mensual"
+        pago_mensual.Width = 150
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(88, 393)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(112, 34)
+        Button1.TabIndex = 2
+        Button1.Text = "Ver Detalle"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(329, 393)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(158, 34)
+        Button2.TabIndex = 3
+        Button2.Text = "Nuevo Operario"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(596, 393)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(158, 34)
+        Button3.TabIndex = 4
+        Button3.Text = "Nuevo Gerente"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Location = New Point(346, 464)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(112, 34)
+        Button4.TabIndex = 5
+        Button4.Text = "Eliminar"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(115, 473)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(142, 25)
+        Label2.TabIndex = 6
+        Label2.Text = "Total Empleados"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(614, 469)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(117, 25)
+        Label3.TabIndex = 7
+        Label3.Text = "Total Nomina"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(831, 593)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(DataGridView1)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents nombre As DataGridViewTextBoxColumn
+    Friend WithEvents rfc As DataGridViewTextBoxColumn
+    Friend WithEvents tipo As DataGridViewTextBoxColumn
+    Friend WithEvents departamento As DataGridViewTextBoxColumn
+    Friend WithEvents pago_mensual As DataGridViewTextBoxColumn
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+
+End Class
